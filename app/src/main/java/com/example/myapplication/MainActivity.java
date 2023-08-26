109a0fd0e2e0d14db9186790eb31fc232b315985
prakassh-me <prakassh@pm.me>
2023-08-26 00:10:35 -0700

    Modified code canges

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -7,6 +7,11 @@ import android.content.ContentResolver;
 import android.content.ContentValues;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
@@ -15,6 +20,7 @@ import android.provider.DocumentsContract;
 import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.File;
@@ -26,13 +32,101 @@ import java.util.Objects;
 import android.Manifest;
 public class MainActivity extends AppCompatActivity {
     Button ssbtn;
+    TextView sensonAngle;
     File path;
+
+    //shake event
+    private float mAccel;
+    private float mAccelCurrent;
+    private float mAccelLast;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ssbtn = findViewById(R.id.ssbtn);
+        sensonAngle = findViewById(R.id.sensonAngle);
+
+        SensorManager sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+
+        Sensor shakeeventsensor =
+                sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
+        Sensor rotationVectorSensor =
+                sensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR);
+
+        mAccel = 10f;
+        mAccelCurrent = sensorManager.GRAVITY_EARTH;
+        mAccelLast = sensorManager.GRAVITY_EARTH;
+
+        SensorEventListener seslistener = new SensorEventListener() {
+            @Override
+            public void onSensorChanged(SensorEvent sensorEvent) {
+                    float x = sensorEvent.values[0];
+                    float y = sensorEvent.values[1];
+                    float z = sensorEvent.values[2];
+                    mAccelLast = mAccelCurrent;
+                    mAccelCurrent = (float) Math.sqrt((double) (z * z));
+                    float delta = mAccelCurrent - mAccelLast;
+                    mAccel = mAccel * 0.9f + delta;
+                    if (mAccel > 12) {
+                        Toast.makeText(getApplicationContext(), "Shake event detected in z-axis" + "current" + mAccelCurrent + "mAccel" + mAccel, Toast.LENGTH_SHORT).show();
+                    }
+
+            }
+
+            @Override
+            public void onAccuracyChanged(Sensor sensor, int i) {
+
+            }
+        };
+
+        // Create a listener
+        SensorEventListener rvListener = new SensorEventListener() {
+            @Override
+            public void onSensorChanged(SensorEvent sensorEvent) {
+                // More code goes here
+                float[] rotationMatrix = new float[16];
+                SensorManager.getRotationMatrixFromVector(
+                        rotationMatrix, sensorEvent.values);
+
+                // Remap coordinate system
+                float[] remappedRotationMatrix = new float[16];
+                SensorManager.remapCoordinateSystem(rotationMatrix,
+                        SensorManager.AXIS_X,
+                        SensorManager.AXIS_Z,
+                        remappedRotationMatrix);
+// Convert to orientations
+                float[] orientations = new float[3];
+                SensorManager.getOrientation(remappedRotationMatrix, orientations);
+                for (int i = 0; i < 3; i++) {
+                    orientations[i] = (float) (Math.toDegrees(orientations[i]));
+                }
+                sensonAngle.setText(Float.toString(Math.round(orientations[2])));
+                if (orientations[2] > 45) {
+                    getWindow().getDecorView().setBackgroundColor(getResources().getColor(R.color.yellow_300));
+                } else if (orientations[2] < -45) {
+                    getWindow().getDecorView().setBackgroundColor(getResources().getColor(R.color.purple_300));
+                } else if (Math.abs(orientations[2]) < 10) {
+                    getWindow().getDecorView().setBackgroundColor(Color.WHITE);
+                }
+            }
+
+            @Override
+            public void onAccuracyChanged(Sensor sensor, int i) {
+
+            }
+
+            ;
+        };
+// Register it
+        sensorManager.registerListener(rvListener,
+                rotationVectorSensor, SensorManager.SENSOR_DELAY_NORMAL);
+
+        // Register it
+        sensorManager.registerListener(seslistener,
+                shakeeventsensor, SensorManager.SENSOR_DELAY_NORMAL);
+
         ssbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
